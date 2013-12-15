40da24e24bfe020e9e6379d8cd09e9b7b939b0c2
Kelvin Gu <gu.kelvin@gmail.com>
2013-12-14 23:01:29 -0800

    just realized that keeping method source is WRONG approach. Abandoning half-finished code

--- src/main/java/com/github/memoize/Tester.java
+++ src/main/java/com/github/memoize/Tester.java
@@ -1,31 +1,26 @@
 package com.github.memoize;
 
 import com.github.memoize.core.GitMemoizer;
-import com.github.memoize.core.StaticAnalysisUtils;
+import com.github.memoize.core.MethodSources;
 import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 import org.apache.log4j.BasicConfigurator;
 
-import java.io.File;
 import java.lang.reflect.Method;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.*;
 
 public class Tester {
 
     private static Logger logger = Logger.getLogger(Tester.class);
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws InterruptedException {
         BasicConfigurator.configure();
         Logger.getRootLogger().setLevel(Level.DEBUG);
 
-        //Method[] targetMethods = GitMemoizer.class.getMethods();
-        //Method targetMethod = targetMethods[0];
-        //StaticAnalysisUtils.extractMethodDefinition(targetMethod, "hello");
+        Method[] targetMethods = GitMemoizer.class.getMethods();
+        Method targetMethod = targetMethods[0];
 
-        //Calculator calc = new Calculator();
-        //logger.debug("First result: " + calc.sum(1, 2));
-        //logger.debug("Second result: " + calc.sum(1, 2));
+        Calculator calc = new Calculator();
+        logger.debug("First result: " + calc.sum(1, 2));
+        logger.debug("Second result: " + calc.sum(1, 2));
     }
 }
