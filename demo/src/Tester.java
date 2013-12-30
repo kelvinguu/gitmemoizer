c5829c42897ebce39f9bac02c5d98a172d48350d
Kelvin Gu <gu.kelvin@gmail.com>
2013-12-29 19:45:20 -0800

    configure logger in Tester.java for demo

--- demo/src/Tester.java
+++ demo/src/Tester.java
@@ -1,10 +1,15 @@
 import org.apache.log4j.Logger;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Level;
 
 public class Tester {
 
-    private static Logger logger = Logger.getLogger(Tester.class);
+    private static Logger logger;
 
     public static void main(String[] args) throws Exception {
+        BasicConfigurator.configure();
+        Logger.getRootLogger().setLevel(Level.DEBUG);
+        logger = Logger.getLogger(Tester.class);
 
         Calculator calc = new Calculator();
         logger.debug("First result: " + calc.sum(1, 2));
