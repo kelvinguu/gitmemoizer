75b975ed670c204ebe3cd67cd3523183467269c7
Kelvin Gu <gu.kelvin@gmail.com>
2014-01-02 14:25:32 -0800

    refactored GitMemoizerTests so that all methods share initial joinPoint mocking

--- src/test/java/com/github/memoize/core/GitMemoizerTests.java
+++ src/test/java/com/github/memoize/core/GitMemoizerTests.java
@@ -19,19 +19,43 @@ public class GitMemoizerTests {
 
     // Integration tests for GitMemoizer.
 
+    // test class whose method is called
+    private class Foo {
+        @Memoizable
+        public double bar(double a, double b) {
+            return Math.pow(a, b);
+        }
+    }
+
     private GitMemoizer gitMemoizer;
+    private ProceedingJoinPoint joinPoint;
 
     @Rule
-    // TODO: is this right syntax? Needs to be static?
+    // TODO: use temporary repo here
     public ExternalResource resource = new ExternalResource() {
 
         @Override
         protected void before() throws Throwable {
+            // initialize GitMemoizer
             File repoPath = new File("/Users/Kelvin/Dropbox/projects/memoize/code/.git");
             File cachePath = new File("/Users/Kelvin/Desktop/memo_cache");
             boolean checkCommit = false;
             Map<Object, Object> cache = new HashMap<Object, Object>();
             gitMemoizer = new GitMemoizer(repoPath, cache, checkCommit);
+
+            // mock joinPoint, giving it a single method
+            MethodSignature methodSignature = mock(MethodSignature.class);
+            Method method = Foo.class.getMethod("bar", double.class, double.class);
+            when(methodSignature.getMethod()).thenReturn(method);
+
+            joinPoint = mock(ProceedingJoinPoint.class);
+            when(joinPoint.getSignature()).thenReturn(methodSignature);
+
+        }
+
+        protected void after() {
+            gitMemoizer = null;
+            joinPoint = null;
         }
 
     };
@@ -44,21 +68,7 @@ public class GitMemoizerTests {
         double b = 20;
         Object[] args = {a, b};
 
-        // test class whose method is called
-        class Foo {
-            @Memoizable
-            public double bar(double a, double b) {
-                return Math.pow(a, b);
-            }
-        }
-
-        // mock joinPoint
-        MethodSignature methodSignature = mock(MethodSignature.class);
-        Method method = Foo.class.getMethod("bar", double.class, double.class);
-        when(methodSignature.getMethod()).thenReturn(method);
-
-        ProceedingJoinPoint joinPoint = mock(ProceedingJoinPoint.class);
-        when(joinPoint.getSignature()).thenReturn(methodSignature);
+        // mock joinPoint further
         when(joinPoint.getArgs()).thenReturn(args);
         when(joinPoint.proceed()).thenReturn(new Foo().bar(a,b));
 
