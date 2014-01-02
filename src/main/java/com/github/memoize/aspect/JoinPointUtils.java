92e5041fcf8fa60d816dc1d8ae50e37fa3d1ea56
Kelvin Gu <gu.kelvin@gmail.com>
2014-01-02 15:52:48 -0800

    JoinPointUtils handles general JoinPoints now

--- src/main/java/com/github/memoize/aspect/JoinPointUtils.java
+++ src/main/java/com/github/memoize/aspect/JoinPointUtils.java
@@ -1,16 +1,16 @@
 package com.github.memoize.aspect;
 
-import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.reflect.MethodSignature;
 import java.lang.reflect.Method;
 
 public class JoinPointUtils {
 
-    public static Class getClass(ProceedingJoinPoint joinPoint) {
+    public static Class getClass(JoinPoint joinPoint) {
         return joinPoint.getTarget().getClass();
     }
 
-    public static Method getMethod(ProceedingJoinPoint joinPoint) {
+    public static Method getMethod(JoinPoint joinPoint) {
         MethodSignature signature = (MethodSignature) joinPoint.getSignature();
         return signature.getMethod();
     }
