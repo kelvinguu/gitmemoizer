525df70291f9cd40d998a608b49b274fe517f23d
Kelvin Gu <gu.kelvin@gmail.com>
2014-01-04 13:31:29 -0800

    simplified Demo.java

--- demo/src/Demo.java
+++ demo/src/Demo.java
@@ -1,18 +1,19 @@
-import org.apache.log4j.Logger;
 import com.github.memoize.aspect.MemoConfig;
+import com.github.memoize.aspect.Memoizable;
 
 public class Demo {
 
-    private static Logger logger;
-
-    @MemoConfig(repoPath="/Users/Kelvin/Dropbox/projects/memoize/code/.git",
-        cachePath ="/Users/Kelvin/Desktop/memocache")
+    @MemoConfig(repoPath=".git", cachePath="memocache", logPath="memolog.txt")
     public static void main(String[] args) throws Exception {
 
-        Calculator calc = new Calculator();
-        System.out.println("First result: " + calc.sum(1, 2));
-        System.out.println("Second result: " + calc.sum(1, 2));
+        System.out.println(exclaim("end the war"));
+        // prints END THE WAR
 
-        System.out.println("Object name: " + calc.getName());
     }
+
+    @Memoizable
+    public static String exclaim(String input) {
+        return input.toUpperCase() + "!!!";
+    }
+
 }
