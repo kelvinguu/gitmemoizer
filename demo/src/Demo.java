a958fbc02f295bbd91221453b563bcc0a005895f
Kelvin Gu <gu.kelvin@gmail.com>
2014-01-06 20:17:25 -0800

    added some saved results from a previous commit Demo.java has been modified: exclaim() now appends exclamation marks after the input

--- demo/src/Demo.java
+++ demo/src/Demo.java
@@ -7,13 +7,18 @@ public class Demo {
     public static void main(String[] args) throws Exception {
 
         System.out.println(exclaim("end the war"));
-        // prints END THE WAR
+        // prints END THE WAR!!!
 
     }
 
+    // Add a 'version' argument to @Memoizable to use results from a previous commit
+    // The older version should return input.toUpperCase()
+    // (without exclamation marks)
+
+    // @Memoizable(version="0b834c49dee1de5c2c86d0bcda6a1b9b2f77e971")
     @Memoizable
     public static String exclaim(String input) {
-        return input.toUpperCase();
+        return input.toUpperCase() + "!!!";
     }
 
 }
