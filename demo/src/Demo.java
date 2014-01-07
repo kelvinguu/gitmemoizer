0b834c49dee1de5c2c86d0bcda6a1b9b2f77e971
Kelvin Gu <gu.kelvin@gmail.com>
2014-01-06 20:09:34 -0800

    Couldn’t add another .git inside the main repo, so Demo.java now just uses the main repo’s .git.

--- demo/src/Demo.java
+++ demo/src/Demo.java
@@ -3,7 +3,7 @@ import com.github.memoize.aspect.Memoizable;
 
 public class Demo {
 
-    @MemoConfig(repoPath=".git", cachePath="memocache", logPath="memolog.txt")
+    @MemoConfig(repoPath="../.git", cachePath="memocache", logPath="memolog.txt")
     public static void main(String[] args) throws Exception {
 
         System.out.println(exclaim("end the war"));
@@ -13,7 +13,7 @@ public class Demo {
 
     @Memoizable
     public static String exclaim(String input) {
-        return input.toUpperCase() + "!!!";
+        return input.toUpperCase();
     }
 
 }
